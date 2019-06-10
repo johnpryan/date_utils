6980bba9e113ecd6f6506be733485d6b3af9239e
Phill Wiggins <phill.wiggins@gmail.com>
2019-06-10 17:53:59 +0100

    Update date_utils.dart
    Lint tidy
    Added start and end day manipulation

--- lib/date_utils.dart
+++ lib/date_utils.dart
@@ -3,86 +3,91 @@ library utils;
 import "package:intl/intl.dart";
 
 class Utils {
-  static final DateFormat _monthFormat = new DateFormat("MMMM yyyy");
-  static final DateFormat _dayFormat = new DateFormat("dd");
-  static final DateFormat _firstDayFormat = new DateFormat("MMM dd");
-  static final DateFormat _fullDayFormat = new DateFormat("EEE MMM dd, yyyy");
-  static final DateFormat _apiDayFormat = new DateFormat("yyyy-MM-dd");
+
+  static final DateFormat _monthFormat = DateFormat('MMMM yyyy');
+  static final DateFormat _dayFormat = DateFormat('dd');
+  static final DateFormat _firstDayFormat = DateFormat('MMM dd');
+  static final DateFormat _fullDayFormat = DateFormat('EEE MMM dd, yyyy');
+  static final DateFormat _apiDayFormat = DateFormat('yyyy-MM-dd');
 
   static String formatMonth(DateTime d) => _monthFormat.format(d);
+
   static String formatDay(DateTime d) => _dayFormat.format(d);
+
   static String formatFirstDay(DateTime d) => _firstDayFormat.format(d);
+
   static String fullDayFormat(DateTime d) => _fullDayFormat.format(d);
+
   static String apiDayFormat(DateTime d) => _apiDayFormat.format(d);
 
-  static const List<String> weekdays = const [
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat"
+  static List<String> weekdays = <String>[
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat'
   ];
 
   /// The list of days in a given month
   static List<DateTime> daysInMonth(DateTime month) {
-    var first = firstDayOfMonth(month);
-    var daysBefore = first.weekday;
-    var firstToDisplay = first.subtract(new Duration(days: daysBefore));
-    var last = Utils.lastDayOfMonth(month);
+    final DateTime first = firstDayOfMonth(month);
+    final int daysBefore = first.weekday;
+    final DateTime firstToDisplay = first.subtract(Duration(days: daysBefore));
+    final DateTime last = Utils.lastDayOfMonth(month);
 
-    var daysAfter = 7 - last.weekday;
+    int daysAfter = 7 - last.weekday;
 
     // If the last day is sunday (7) the entire week must be rendered
     if (daysAfter == 0) {
       daysAfter = 7;
     }
 
-    var lastToDisplay = last.add(new Duration(days: daysAfter));
-    return daysInRange(firstToDisplay, lastToDisplay).toList();
+    final DateTime lastToDisplay = last.add(Duration(days: daysAfter));
+    return Utils.daysInRange(firstToDisplay, lastToDisplay).toList();
   }
 
   static bool isFirstDayOfMonth(DateTime day) {
-    return isSameDay(firstDayOfMonth(day), day);
+    return Utils.isSameDay(firstDayOfMonth(day), day);
   }
 
   static bool isLastDayOfMonth(DateTime day) {
-    return isSameDay(lastDayOfMonth(day), day);
+    return Utils.isSameDay(lastDayOfMonth(day), day);
   }
 
   static DateTime firstDayOfMonth(DateTime month) {
-    return new DateTime(month.year, month.month);
+    return DateTime(month.year, month.month);
   }
 
   static DateTime firstDayOfWeek(DateTime day) {
     /// Handle Daylight Savings by setting hour to 12:00 Noon
     /// rather than the default of Midnight
-    day = new DateTime.utc(day.year, day.month, day.day, 12);
+    day = DateTime.utc(day.year, day.month, day.day, 12);
 
     /// Weekday is on a 1-7 scale Monday - Sunday,
     /// This Calendar works from Sunday - Monday
-    var decreaseNum = day.weekday % 7;
-    return day.subtract(new Duration(days: decreaseNum));
+    final int decreaseNum = day.weekday % 7;
+    return day.subtract(Duration(days: decreaseNum));
   }
 
   static DateTime lastDayOfWeek(DateTime day) {
     /// Handle Daylight Savings by setting hour to 12:00 Noon
     /// rather than the default of Midnight
-    day = new DateTime.utc(day.year, day.month, day.day, 12);
+    day = DateTime.utc(day.year, day.month, day.day, 12);
 
     /// Weekday is on a 1-7 scale Monday - Sunday,
     /// This Calendar's Week starts on Sunday
-    var increaseNum = day.weekday % 7;
-    return day.add(new Duration(days: 7 - increaseNum));
+    final int increaseNum = day.weekday % 7;
+    return day.add(Duration(days: 7 - increaseNum));
   }
 
   /// The last day of a given month
   static DateTime lastDayOfMonth(DateTime month) {
-    var beginningNextMonth = (month.month < 12)
-        ? new DateTime(month.year, month.month + 1, 1)
-        : new DateTime(month.year + 1, 1, 1);
-    return beginningNextMonth.subtract(new Duration(days: 1));
+    final DateTime beginningNextMonth = (month.month < 12)
+        ? DateTime(month.year, month.month + 1, 1)
+        : DateTime(month.year + 1, 1, 1);
+    return beginningNextMonth.subtract(Duration(days: 1));
   }
 
   /// Returns a [DateTime] for each day the given range.
@@ -90,15 +95,15 @@ class Utils {
   /// [start] inclusive
   /// [end] exclusive
   static Iterable<DateTime> daysInRange(DateTime start, DateTime end) sync* {
-    var i = start;
-    var offset = start.timeZoneOffset;
+    DateTime i = start;
+    Duration offset = start.timeZoneOffset;
     while (i.isBefore(end)) {
       yield i;
-      i = i.add(new Duration(days: 1));
-      var timeZoneDiff = i.timeZoneOffset - offset;
+      i = i.add(Duration(days: 1));
+      final Duration timeZoneDiff = i.timeZoneOffset - offset;
       if (timeZoneDiff.inSeconds != 0) {
         offset = i.timeZoneOffset;
-        i = i.subtract(new Duration(seconds: timeZoneDiff.inSeconds));
+        i = i.subtract(Duration(seconds: timeZoneDiff.inSeconds));
       }
     }
   }
@@ -111,35 +116,38 @@ class Utils {
   static bool isSameWeek(DateTime a, DateTime b) {
     /// Handle Daylight Savings by setting hour to 12:00 Noon
     /// rather than the default of Midnight
-    a = new DateTime.utc(a.year, a.month, a.day);
-    b = new DateTime.utc(b.year, b.month, b.day);
+    a = DateTime.utc(a.year, a.month, a.day);
+    b = DateTime.utc(b.year, b.month, b.day);
 
-    var diff = a.toUtc().difference(b.toUtc()).inDays;
+    final int diff = a
+        .toUtc()
+        .difference(b.toUtc())
+        .inDays;
     if (diff.abs() >= 7) {
       return false;
     }
 
-    var min = a.isBefore(b) ? a : b;
-    var max = a.isBefore(b) ? b : a;
-    var result = max.weekday % 7 - min.weekday % 7 >= 0;
+    final DateTime min = a.isBefore(b) ? a : b;
+    final DateTime max = a.isBefore(b) ? b : a;
+    final bool result = max.weekday % 7 - min.weekday % 7 >= 0;
     return result;
   }
 
   static DateTime previousMonth(DateTime m) {
-    var year = m.year;
-    var month = m.month;
+    int year = m.year;
+    int month = m.month;
     if (month == 1) {
       year--;
       month = 12;
     } else {
       month--;
     }
-    return new DateTime(year, month);
+    return DateTime(year, month);
   }
 
   static DateTime nextMonth(DateTime m) {
-    var year = m.year;
-    var month = m.month;
+    int year = m.year;
+    int month = m.month;
 
     if (month == 12) {
       year++;
@@ -147,14 +155,27 @@ class Utils {
     } else {
       month++;
     }
-    return new DateTime(year, month);
+    return DateTime(year, month);
   }
 
   static DateTime previousWeek(DateTime w) {
-    return w.subtract(new Duration(days: 7));
+    return w.subtract(Duration(days: 7));
   }
 
   static DateTime nextWeek(DateTime w) {
-    return w.add(new Duration(days: 7));
+    return w.add(Duration(days: 7));
+  }
+
+
+  static DateTime startOfDay(DateTime d) {
+    return d.subtract(Duration(hours: -d.hour,
+        minutes: -d.minute,
+        milliseconds: -d.millisecond,
+        microseconds: -d.microsecond));
+  }
+
+  static DateTime endOfDay(DateTime d) {
+    return d.subtract(Duration(
+        hours: 23, minutes: 59, milliseconds: 999, microseconds: 9999999));
   }
 }
